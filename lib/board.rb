ce463ea93c9db20f5b48f49cd41c28ae10450db4
chris <cdresher2010@gmail.com>
2021-10-25 22:19:27 -0400

    working on adding stones to each cup

--- lib/board.rb
+++ lib/board.rb
@@ -2,16 +2,37 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @player1 = name1
+    @player2 = name2
+    @cups = Array.new(14) {Array.new(4)}
+    place_stones()
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    for cupIdx in 0...@cups.length
+      if cupIdx == 6 || cupIdx == 13
+        @cups[cupIdx].clear
+      else
+        @cups[cupIdx].fill(:stone)
+      end
+    end
   end
 
   def valid_move?(start_pos)
+    if (start_pos < 0 || start_pos > 13)
+      raise 'Invalid starting cup'
+    end
+    if @cups[start_pos].empty?
+      raise 'Starting cup is empty'
+    end
   end
 
   def make_move(start_pos, current_player_name)
+    @cups[start_pos].each do |stone|
+      
+    end
+    @cups[start_pos].clear # pick up all stones in the cup
   end
 
   def next_turn(ending_cup_idx)
